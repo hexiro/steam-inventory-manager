31a0b7eded259b4ea05b6d7e8c659f3e421d84d9
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-09-29 17:44:46 -0400

    feat: make config easy to use :+1:

--- steam-inventory-manager/config.py
+++ steam-inventory-manager/config.py
@@ -1,11 +1,62 @@
 import pathlib
+from dataclasses import dataclass
+from enum import Enum
+from typing import List
 
 import yaml
 
 
-config_file = pathlib.Path(__file__).parents[2] / "config.yaml"
+# for now im leaving these here instead of in "types.py"
+# i might change my mind
+
+@dataclass
+class MainAccount:
+    username: str
+    password: str
+    shared_secret: str
+    identity_secret: str
+
+
+class Priority(Enum):
+    CASES = "cases"
+    GRAFFITIES = "graffities"
+    STICKERS = "stickers"
+    AGENTS = "agents"
+    KEYS = "keys"
+
+
+@dataclass
+class AlternateAccount:
+    username: str
+    password: str
+    shared_secret: str
+    identity_secret: str
+    priority: List[Priority] = None
+
+
+@dataclass
+class Options:
+    min_price: float
+    disallow_graffities: bool
+
+config_file = pathlib.Path(__file__).parents[1] / "config.yaml"
 config = yaml.safe_load(config_file.read_text(encoding="utf-8"))
 
-managed_account = config["managed-account"]
-secondary_accounts = config["secondary-accounts"]
-options = config["options"]
+main_account = MainAccount(
+    username=config["main-account"]["username"],
+    password=config["main-account"]["password"],
+    shared_secret=config["main-account"]["shared-secret"],
+    identity_secret=config["main-account"]["identity-secret"]
+)
+alternate_accounts = [AlternateAccount(
+    username=acc["username"],
+    password=acc["password"],
+    shared_secret=acc["shared-secret"],
+    identity_secret=acc["identity-secret"],
+    priority=[Priority(p) for p in acc["priority"]]
+) for acc in config["alternate-accounts"]]
+
+options = Options(
+    min_price=config["options"]["min-price"],
+    disallow_graffities=config["options"]["disallow-graffities"],
+)
