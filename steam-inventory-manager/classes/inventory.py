f394d99ce56f8bdb670dad55a3ded547254568e3
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-10-13 20:35:50 -0400

    fix: bug that prevented duplicate items from being traded

--- steam-inventory-manager/classes/inventory.py
+++ steam-inventory-manager/classes/inventory.py
@@ -14,28 +14,30 @@ class Inventory:
 
     @cached_property
     def items(self) -> List[Item]:
-        resp = requests.get(f"https://steamcommunity.com/inventory/{self.steam_id.as_64}/730/2?l=english&count=5000").json()
+        resp = requests.get(
+            f"https://steamcommunity.com/inventory/{self.steam_id.as_64}/730/2?l=english&count=5000").json()
         assets: dict = resp["assets"]
         descriptions: dict = resp["descriptions"]
 
         items: List[Item] = []
 
-        for item in descriptions:
+        for asset in assets:
+            # ex. {'appid': 730, 'contextid': '2', 'assetid': '23603986921', 'classid': '4593998508', 'instanceid':
+            # '519977179', 'amount': '1'}
+            classid = asset["classid"]
+            description = next((x for x in descriptions if x["classid"] == classid), None)
             # this means that the item isn't currently tradable.
             # sometimes the item will never be tradable and other times it will be tradable after 7 days.
-            if item["tradable"] == 0:
-                continue
-            classid = item["classid"]
-            asset = next((x for x in assets if x["classid"] == classid), None)
-            if not asset:
+            if description["tradable"] == 0:
                 continue
 
             type_values = [x.value for x in Type]
             # there may be better ways to parse this.
-            raw_exterior = next((x["value"].split("Exterior: ")[-1] for x in item["descriptions"] if x["value"].startswith("Exterior: ")), None)
-            raw_type = next((x["localized_tag_name"] for x in item["tags"] if x["category"] == "Type"), None)
+            raw_exterior = next((x["value"].split("Exterior: ")[-1] for x in description["descriptions"] if
+                                 x["value"].startswith("Exterior: ")), None)
+            raw_type = next((x["localized_tag_name"] for x in description["tags"] if x["category"] == "Type"), None)
 
-            name = item["name"]
+            name = description["name"]
             # right now, only csgo is supported so these are hard coded
             appid = 730
             contextid = "2"
@@ -60,8 +62,3 @@ class Inventory:
     @property
     def items_to_trade(self):
         return [item for item in self.items if item.should_be_traded]
-
-
-if __name__ == "__main__":
-    inv = Inventory(SteamID(76561199033382814))
-    pprint(inv.items)
