cef39f404616bf0c8e8d60f990eb8fc6c071a896
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-10-15 16:49:13 -0400

    chore: custom config error messages

--- steam-inventory-manager/config.py
+++ steam-inventory-manager/config.py
@@ -4,30 +4,38 @@ import yaml
 
 from .classes import Account
 from .types import Type, Options
+from .exceptions import ConfigurationError
 
-config_file = pathlib.Path(__file__).parents[1] / "config.yaml"
-config = yaml.safe_load(config_file.read_text(encoding="utf-8"))
+try:
+    config_file = pathlib.Path(__file__).parents[1] / "config.yaml"
+    config = yaml.safe_load(config_file.read_text(encoding="utf-8"))
+    print(config)
+except yaml.YAMLError:
+    raise ConfigurationError("failed to load config.yaml")
 
-main_account = Account(
-    username=config["main-account"]["username"],
-    password=config["main-account"]["password"],
-    shared_secret=config["main-account"]["shared-secret"],
-    identity_secret=config["main-account"]["identity-secret"]
-)
-alternate_accounts = [Account(
-    username=acc["username"],
-    password=acc["password"],
-    shared_secret=acc["shared-secret"],
-    identity_secret=acc["identity-secret"],
-    priority=[Type(p.title() if p != "SMG" else p) for p in acc["priority"]] if "priority" in acc else None
-) for acc in config["alternate-accounts"]]
+try:
+    main_account = Account(
+        username=config["main-account"]["username"],
+        password=config["main-account"]["password"],
+        shared_secret=config["main-account"]["shared-secret"],
+        identity_secret=config["main-account"]["identity-secret"]
+    )
+    alternate_accounts = [Account(
+        username=acc["username"],
+        password=acc["password"],
+        shared_secret=acc["shared-secret"],
+        identity_secret=acc["identity-secret"],
+        priority=[Type(p.title() if p != "SMG" else p) for p in acc["priority"]] if "priority" in acc else None
+    ) for acc in config["alternate-accounts"]]
 
-options = Options(
-    min_price=config["options"]["min-price"],
-    always_trade_graffities=config["options"].get("always-trade-graffities", False),
-    always_trade_stickers=config["options"].get("always-trade-stickers", False),
-    always_trade_agents=config["options"].get("always-trade-agents", False),
-    always_trade_containers=config["options"].get("always-trade-containers", False),
-    always_trade_collectibles=config["options"].get("always-trade-collectibles", False),
-    always_trade_patches=config["options"].get("always-trade-patches", False),
-)
+    options = Options(
+        min_price=config["options"]["min-price"],
+        always_trade_graffities=config["options"].get("always-trade-graffities", False),
+        always_trade_stickers=config["options"].get("always-trade-stickers", False),
+        always_trade_agents=config["options"].get("always-trade-agents", False),
+        always_trade_containers=config["options"].get("always-trade-containers", False),
+        always_trade_collectibles=config["options"].get("always-trade-collectibles", False),
+        always_trade_patches=config["options"].get("always-trade-patches", False),
+    )
+except KeyError as e:
+    raise ConfigurationError(f"key, {str(e)} not present in config.yaml") from None
