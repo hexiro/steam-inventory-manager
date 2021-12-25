5de837c9b08e928ba70224ff89af072c0008627b
Hexiro <42787085+Hexiro@users.noreply.github.com>
2021-12-24 21:53:42 -0500

    refactor: add more logging & other small changes

--- steam-inventory-manager/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import logger
-
-logger.setup()
--- steam-inventory-manager/__main__.py
+++ steam-inventory-manager/__main__.py
@@ -5,13 +5,15 @@ from collections import defaultdict
 from typing import TYPE_CHECKING
 
 from .account import Account
-from .inventory import Inventory
 from .config import main_account, alternate_accounts
+from .inventory import Inventory
+from .logger import setup
 from .utils import parse_priorities
 
 if TYPE_CHECKING:
     from .datatypes import ItemType, Item
 
+setup()
 logger = logging.getLogger(__name__)
 
 
@@ -23,25 +25,20 @@ class SteamInventoryManager:
             shared_secret=main_account["shared-secret"],
             identity_secret=main_account["identity-secret"],
         )
-        logger.debug(f"{main_account=}")
         self.main_account.login()
 
         self.alternate_accounts: list[Account] = []
         for config_acc in alternate_accounts:
-            self.alternate_accounts.append(
-                Account(
-                    username=config_acc["username"],
-                    password=config_acc["password"],
-                    shared_secret=config_acc["shared-secret"],
-                    priorities=parse_priorities(config_acc.get("priorities")),
-                )
+            acc = Account(
+                username=config_acc["username"],
+                password=config_acc["password"],
+                shared_secret=config_acc["shared-secret"],
+                priorities=parse_priorities(config_acc.get("priorities")),
             )
-
-        for index, acc in enumerate(self.alternate_accounts, start=1):
-            logger.debug(f"alternate account #{index}={acc}")
             acc.login()
+            self.alternate_accounts.append(acc)
 
-        self.inventory: Inventory = Inventory(self.main_account.steam_id64)
+        self.inventory = Inventory(self.main_account.steam_id64)
 
     def which_alternate_account(self, item_type: ItemType):
         """
@@ -53,12 +50,17 @@ class SteamInventoryManager:
                 return acc
         return self.alternate_accounts[0]
 
-    def main(self):
+    def main(self) -> None:
+        logger.info(f"Main account: {self.main_account.username}")
+
+        for index, acc in enumerate(self.alternate_accounts, start=1):
+            logger.info(f"Alternate account #{index}: {acc.username}")
+
+        logger.info("All accounts logged in!")
+
         if not self.inventory.items_to_trade:
             logger.info("Found no items to trade.")
             return
-
-        logger.debug(f"items_to_trade={self.inventory.items_to_trade}")
 
         trade_offers: dict[Account, list[Item]] = defaultdict(list)
 
@@ -67,20 +69,22 @@ class SteamInventoryManager:
             trade_offers[acc].append(item)
 
         for acc, items in trade_offers.items():
-            # trade_id = self.main_account.trade(
-            #     partner=acc,
-            #     me=[item.trade_asset for item in items],
-            # )
-            logger.info(f"Opening trade offer with {acc.username}:")
-            logger.info(f"items={[item.name for item in items]}")
-            # acc.accept_trade(
-            #     partner=self.main_account,
-            #     trade_id=trade_id,
-            # )
-        logger.info(
-            f"Successfully opened {len(trade_offers)} trade offers with {len(self.inventory.items_to_trade)} total "
-            "items."
-        )
+            trade_id = self.main_account.trade(
+                partner=acc,
+                me=[item.trade_asset for item in items],
+            )
+            logger.info(f"Opening trade offer with: {acc.username}")
+            logger.info(f"Items being traded: {', '.join(i.market_name for i in self.inventory.items_to_trade)}")
+            acc.accept_trade(
+                partner=self.main_account,
+                trade_id=trade_id,
+            )
+        len_offers = len(trade_offers)
+        len_items = len(self.inventory.items_to_trade)
+        offers_noun = "offers" if len_offers > 1 else "offer"
+        items_noun = "items" if len_items > 1 else "item"
+
+        logger.info(f"Successfully opened {len_offers} trade {offers_noun} with {len_offers} total {items_noun}.")
 
 
 if __name__ == "__main__":
